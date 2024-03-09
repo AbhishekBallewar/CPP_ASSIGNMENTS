4dbd6dd1185368bb8cab7dcd2af90cced660b5f9
d5_83636_Abhishek <abhishekballewar@gmail.com>
2024-03-09 21:19:29 +0530

    Assigment 3 ques 2

---
+++ Assign3_2.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+  int top;
+  int *arr;
+  int size;
+  public:
+  Stack(){
+     top = -1;
+  }
+  Stack(int size){
+    top = -1;
+    arr = new int(size);
+    this->size=size;
+  }
+  int push(int num){
+    if(isFull()){
+      cout<<"Stack is full"<<endl;
+      return -1;
+    }  
+    arr[++top]=num;
+    return -1;
+  }
+  int pop(){
+   if(isEmpty()){
+    cout<<"Stack is Empty"<<endl;
+    return -1;
+   }
+   
+    return arr[top--];
+  }
+  int peek(){
+    if(isEmpty()){
+    cout<<"Stack is Empty"<<endl;
+    return -1;
+   }
+   return arr[top];
+  }
+  bool isEmpty(){
+       return (top==-1);
+  }
+  bool isFull(){
+         return (top==(size-1)); 
+  }
+  ~Stack(){
+    delete []arr;
+  }
+};
+int menu(){
+  int choice;
+    cout<<"0.To Exit"<<endl<<"1.To Push"<<endl;
+    cout<<"2.To Pop"<<endl<<"3.To Peek"<<endl;
+    cout<<endl;
+    
+    cin>>choice;
+    return choice;
+
+}
+int main (void){
+  int choice;
+  int size;
+  cout<<"Enter the size of the array"<<endl;
+  cin>>size;
+  Stack st[size];
+  int num;
+  int poped;
+  int peeked;
+  
+  enum Cases{EXIT=0,PUSH=1,POP,PEEK};
+      while((choice= menu())!=0){
+         switch (Cases(choice))
+         {
+         case PUSH:
+         cout<<"Enter number to be pushed"<<endl;
+         cin>>num;
+          st->push(num);
+          break;
+          case POP:
+           poped = st->pop();
+          if(poped!=-1){
+          cout<<"popped element is"<<poped<<endl;
+          }
+          break;
+          case PEEK:
+          peeked = st->peek();
+         if(peeked!=-1){
+          cout<<"peek element is"<<peeked<<endl;
+          break;
+         
+         default:
+         cout<<"invalid choice"<<endl;
+          break;
+         }
+      }
+     
+}
+}
